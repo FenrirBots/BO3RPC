120d464cfd016e5bc4bf1d647a412c8bcca5944e
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-27 03:40:16 +0100

    Updated build.bat

---
+++ Build/Build.bat
@@ -0,0 +1,47 @@
+@echo off
+cls
+
+setlocal
+
+set BuildTools32=
+set BuildTools64=
+
+if ["%1"]==[""] (
+	echo [BUILD]: No build tools provided, searching known directories
+
+	:: I hate this...
+	if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\" (
+		set BuildTools32="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+		set BuildTools64="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+		echo [BUILD]: Visual Studio Build Tools 2022 found
+	) else (
+		if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\" (
+			set BuildTools32="C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+			set BuildTools64="C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+			echo [BUILD]: Visual Studio Build Tools 2019 found
+		) else (
+			if exist "C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\" (
+				set BuildTools32="C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+				set BuildTools64="C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+				echo [BUILD]: Visual Studio Build Tools 2017 found
+			) else (
+				echo [BUILD]: No build tools found, please provide them as an argument and try again...	
+				goto :pausethenend
+			)
+		)
+	)
+)
+
+call %BuildTools64%
+call cl.exe /nologo /c /W4 /O2 /MD /D_NDEBUG /Fo"Bin\Intermediate\\" /I"../Source" /TP "..\Source\Main.cpp" "..\Source\LeoHook\LeoHook.cpp" "..\Source\Discord\Discord.cpp"
+call link.exe /nologo /dll /subsystem:windows /out:"Bin\RichPresence.dll" "Bin\Intermediate\*.obj"
+
+if ["%ERRORLEVEL%"] NEQ ["0"] (
+	echo An error occoured during the build process...
+)
+
+:pausethenend
+pause
+
+:end
+endlocal
