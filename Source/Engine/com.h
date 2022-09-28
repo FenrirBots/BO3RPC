01aef18b20b00d7325cb5ee7ed1c786e0eed1a35
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-28 03:12:46 +0100

    Added some stuff
    Added the namespace SessionMode
    Added Com::SessionMode::IsMode
    Added Com::SessionMode::GetMode
    
    Added a note people can reference for Com::SessionMode::IsMode

--- Source/Engine/com.h
+++ Source/Engine/com.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Engine/EngineTypes.h"
 
+// Reference for Com_SessionMode_IsMode
+//	MODE_ZOMBIES = 0x0,
+//	MODE_MULTIPLAYER = 0x1,
+//	MODE_CAMPAIGN = 0x2,
+//	MODE_COUNT = 0x3,
+//	MODE_INVALID = 0x3,
+//	MODE_FIRST = 0x0,
+
+
 namespace Com
 {
 	extern bool(__cdecl* IsInGame)();
+
+	namespace SessionMode
+	{
+		extern bool(__fastcall* IsMode)(int);
+		extern uint64(__cdecl* GetMode)();
+	}
 }
