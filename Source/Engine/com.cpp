c3eedac561d3a131d66c7993e5f4662fa59dc5f0
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-28 03:13:03 +0100

    Forgot to add this to the last update

--- Source/Engine/com.cpp
+++ Source/Engine/com.cpp
@@ -1,3 +1,7 @@
 #include "Engine/Com.h"
 
-bool (__cdecl* Com::IsInGame)() = GetAddress<bool(__cdecl*)()>(0x7FF773A58D20);
+bool(__cdecl* Com::IsInGame)() = GetAddress<bool(__cdecl*)()>(0x7FF773A58D20);
+bool(__fastcall* Com::SessionMode::IsMode)(int) = GetAddress<bool(__fastcall*)(int)>(0x7FF773A07DD0);
+
+// This returns a union (I hate unions)
+uint64(__cdecl* Com::SessionMode::GetMode)() = GetAddress<uint64(__cdecl*)()>(0x7FF773A07790);
