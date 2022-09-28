d18b9d0ff5c0c29edb0d86a2a37f9d9c85538365
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-28 03:11:41 +0100

    Put in a note for anyone reading the source

--- Source/Engine/engine.h
+++ Source/Engine/engine.h
@@ -5,3 +5,7 @@
 #include "Engine/LobbySession.h"
 
 // Stuff to look at later 0x7FF7737C60E0
+
+
+// I do know that having them in namespaces is technically wrong since 
+// they wasnt originally like that but i feel like it looks better.
