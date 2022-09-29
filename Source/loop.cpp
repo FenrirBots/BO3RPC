ed1f66d5ecfa808981a9fa54d1690d92c81bb24a
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-29 07:29:30 +0100

    Update loop.cpp

--- Source/loop.cpp
+++ Source/loop.cpp
@@ -1,82 +1,29 @@
 #include "loop.h"
+#include "config.h"
 #include <chrono>
 
 bool Loop::Update = true;
 bool Loop::Running = true;
 bool Loop::ForceUpdate = false;
 
-std::chrono::steady_clock::time_point delta = std::chrono::steady_clock::now();
-
-
-void Loop::GetInput()
-{
-	// Enable Rich Presence
-	if (GetAsyncKeyState(VK_F5) & 0x01)
-	{
-		if (Loop::Update == false)
-		{
-			Discord::Update = true;
-			Loop::Update = true;
-		}
-	}
-
-	// Disable Rich Presence
-	if (GetAsyncKeyState(VK_F6) & 0x01)
-	{
-		if (Loop::Update == true)
-		{
-			Discord::Update = false;
-			Loop::Update = false;
-		}
-	}
-
-	// Clear Presence Data
-	if (GetAsyncKeyState(VK_F7) & 0x01)
-	{
-		Discord::Clear = true;
-	}
-
-	// Reload config from disk
-	if (GetAsyncKeyState(VK_F8))
-	{
-		// This hasnt been implemented yet
-	}
-
-	// Detach from target process
-	if (GetAsyncKeyState(VK_F9))
-	{
-		// This doesnt work with leohook...
-	}
-}
+auto Delta = std::chrono::steady_clock::now();
 
 void Loop::Loop(int localClientNumber)
 {
-	Loop::GetInput();
-
 	if (Loop::Update)
 	{
-		std::chrono::steady_clock timer;
+		std::chrono::steady_clock Time;
 
-		if (std::chrono::duration_cast<std::chrono::seconds>(timer.now() - delta) >= std::chrono::seconds(5))
-		{
-			Discord::RichPresence.lock();
+		auto UpdateTime = std::chrono::seconds(5);
+		if (Com::IsInGame()) {
+			UpdateTime = std::chrono::seconds(30);
+		}
 
-			if (Com::IsInGame())
-			{
-
-				Discord::RichPresence.Presence.instance = 1;
+		if (std::chrono::duration_cast<std::chrono::seconds>(Time.now() - Delta) >= UpdateTime) {
+			if (!Discord::Update) { // This needs to be named better...
+				Discord::UpdatePresence();
 			}
-			else
-			{
-				// Find the function for this
-				Discord::RichPresence.Presence.partySize = 1;
-				Discord::RichPresence.Presence.partyMax = LobbySession::GetMaxClients(0);
-				Discord::RichPresence.Presence.instance = 0;
-			}
-
-			Discord::RichPresence.unlock();
-
-			delta = timer.now();
+			Delta = Time.now();
 		}
 	}
 
@@ -84,13 +31,11 @@ void Loop::Loop(int localClientNumber)
 	return CG::Draw2DInternal(localClientNumber);
 }
 
-DWORD Loop::Attach(LPVOID /* lpvReserved */)
-{
+DWORD Loop::Attach(LPVOID /* lpvReserved */) {
+	Config::LoadConfig();
+
 	if (!LeoHook::Hook((uint64_t)CG::Draw2D, (uint64_t)Loop::Loop))
-	{
-		// Failed to hook, notify the user somehow.
 		return FALSE;
-	}
 
 	return TRUE;
 }
