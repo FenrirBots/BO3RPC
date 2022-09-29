531ab9daedd15243968973333ce1f878ebc5a212
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-29 07:29:21 +0100

    Added a way to parse the json
    Added ParseString and Find
    These together help parse and replace sections of the json string that have temp values such as ${CurrentRound} or ${ZombiesKilled}.
    
    Updated Discord::UpdatePresence to cover all gamemodes and lobby types (Except for stuff such as the gumsmith and Dr.Montys Factory)

--- Source/Discord/discord.cpp
+++ Source/Discord/discord.cpp
@@ -38,6 +38,75 @@ DWORD WINAPI Discord::Loop(LPVOID /*lpvReserved*/)
 	return 1;
 }
 
+
+int find(std::string string, const char* szstring)
+{
+	std::string::iterator pos = std::search(string.begin(), string.end(), std::string(szstring).begin(), std::string(szstring).end(), [](char ch1, char ch2) { return std::toupper(ch1) == std::toupper(ch2); });
+	if (fpos != string.end())
+		return std::distance(text.beign(), pos);
+	return 0;
+}
+
+const char* ParseString(const char* data)
+{
+	std::string string(data);
+
+	while (true)
+	{
+		int pos = 0;
+		
+		pos = find(string, "${CurrentRound}");
+		if (pos > 0)
+		{
+			uint64 DDLState = 0;
+			if (DDL::MoveToName(&*GetAddress<uint64*>(0x7FF774C3F680), &DDLState, "numZombieRounds"))
+			{
+				auto CurrentRound = DDL::GetUInt(&DDLState, &*GetAddress<uint64*>(0x7FF77B506A20)) + 1);
+				string.replace(pos, std::string("${CurrentRound}").length(), std::to_string(CurrentRound));
+			}
+			else
+			{
+				string.replace(pos, std::string("${CurrentRound}").length(), "0");
+			}
+
+			continue;
+		}
+
+		pos = find(string, "${ZombiesKilled");
+		if (pos > 0)
+		{
+			string.replace(pos, std::string("${Deaths}").length(), "0");
+			continue;
+		}
+		
+		pos = find(string, "${Downs}");
+		if (pos > 0)
+		{
+			string.replace(pos, std::string("${Deaths}").length(), "0");
+			continue;
+		}
+
+		`pos = find(string, "${Deaths}");
+		if (pos > 0)
+		{
+			string.replace(pos, std::string("${Deaths}").length(), "0");
+			continue;
+		}
+		
+		pos = find(string, "${MapName}");
+		if (pos > 0)
+		{
+			string.replace(pos, std::string("${MapName}").length(), (const char*)((uint64)GetModuleHandle(0) + 0x194417A2));
+			continue;
+		}
+
+		// If we dont find anything, exit the loop.
+		break;
+	}
+
+	return string.c_str();
+}
+
 void Discord::UpdatePresence()
 {
 	if (!Discord::Update)
@@ -45,6 +114,74 @@ void Discord::UpdatePresence()
 		Discord::RichPresence.lock();
 		auto LobbyMode = *(int*)(LobbySession::GetSession(1) + 0x8);
 		
+		if (Com::IsInGame())
+		{
+
+			switch (LobbyMode)
+			{
+			case 0:
+				if (Com::SessionMode::IsMode(0))
+				{
+					std::string details;
+					std::string state;
+
+					if (LobbySession::GetClientCount(1, 0) <= 1)
+					{
+						state = Config::Config["Zombies"]["Presence"]["State"]["2-4"];
+					}
+					else
+					{
+						state = Config::Config["Zombies"]["Presence"]["State"]["2-4"];
+					}
+
+					Discord::RichPresence.Presence.state = Discord::ParseString(state);
+				}
+				else if (Com::SessionMode::IsMode(1))
+				{
+
+				}
+				else if (Com::SessionMode::IsMode(2))
+				{
+
+				}
+				break;
+			case 1:
+				if (Com::SessionMode::IsMode(0))
+				{
+
+				}
+				else if (Com::SessionMode::IsMode(1))
+				{
+
+				}
+				else if (Com::SessionMode::IsMode(2))
+				{
+
+				}
+				break;
+			case 2:
+				if (Com::SessionMode::IsMode(0))
+				{
+
+				}
+				else if (Com::SessionMode::IsMode(1))
+				{
+
+				}
+				break;
+			case 3:
+				// Arena
+				break;
+			case 4:
+				// Freerun
+				break;
+			default:
+			}
+		}
+		else
+		{
+
+		}
 
 		Discord::RichPresence.unlock();
 	}
