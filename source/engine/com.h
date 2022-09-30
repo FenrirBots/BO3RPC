c1afe65c152acd531b8b8eeed47f3ca0d7aa101f
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-09-30 16:32:14 +0100

    Moved a function

--- source/engine/com.h
+++ source/engine/com.h
@@ -6,10 +6,10 @@ namespace engine
     namespace com
     {
 	    extern bool(__cdecl* isingame)();
+    	extern bool(__fastcall* ismode)(int);
 
 	    namespace sessionmode
 	    {
-	    	extern bool(__fastcall* ismode)(int);
 	    	extern unsigned long long(__cdecl* getmode)();
 	    }
     }
