3ef1eb919980b447282111051bdd84626b9ddf33
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-10-19 05:52:59 +0100

    Update attach.cpp
    Surrounded parser::removeinvalid with a try/catch incase json throws an error when getting the config value.

--- source/threads/attach.cpp
+++ source/threads/attach.cpp
@@ -11,20 +11,25 @@
 DWORD WINAPI threads::attach(LPVOID /* lpvReserved */)
 {
     config::load();
-      parser::addrule("${ping}", parser::callbacks::ping);
-      parser::addrule("${place}", parser::callbacks::place);
-      parser::addrule("${score}", parser::callbacks::score);
-      parser::addrule("${kills}", parser::callbacks::kills);
-      parser::addrule("${assists}", parser::callbacks::assists);
-      parser::addrule("${deaths}", parser::callbacks::deaths);
-      parser::addrule("${downs}", parser::callbacks::downs);
-      parser::addrule("${revives}", parser::callbacks::revives);
-      parser::addrule("${headshots}", parser::callbacks::headshots);
-      parser::addrule("${mapname}", parser::callbacks::mapname);
-      parser::addrule("${fastfile}", parser::callbacks::fastfile);
-      parser::addrule("${round}", parser::callbacks::round);
-      parser::addrule("${username}", parser::callbacks::username);
-      parser::addrule("${sessionmode}", parser::callbacks::sessionmode);
+
+    try { 
+        parser::removeinvalid = config::get()["remove-invalid-rules"].get<bool>();
+    } catch(...) {}
+
+    parser::addrule("${ping}", parser::callbacks::ping);
+    parser::addrule("${place}", parser::callbacks::place);
+    parser::addrule("${score}", parser::callbacks::score);
+    parser::addrule("${kills}", parser::callbacks::kills);
+    parser::addrule("${assists}", parser::callbacks::assists);
+    parser::addrule("${deaths}", parser::callbacks::deaths);
+    parser::addrule("${downs}", parser::callbacks::downs);
+    parser::addrule("${revives}", parser::callbacks::revives);
+    parser::addrule("${headshots}", parser::callbacks::headshots);
+    parser::addrule("${mapname}", parser::callbacks::mapname);
+    parser::addrule("${fastfile}", parser::callbacks::fastfile);
+    parser::addrule("${round}", parser::callbacks::round);
+    parser::addrule("${username}", parser::callbacks::username);
+    parser::addrule("${sessionmode}", parser::callbacks::sessionmode);
 
     if(KIEROFAILED(kiero::init(kiero::RenderType::D3D11))  ||
        KIEROFAILED(kiero::bind(8, (void**)&present, update)))
