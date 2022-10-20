932b328ecb3c1ae4d3e367b2833758d75956063f
Ben Bowman <111782432+FenrirBots@users.noreply.github.com>
2022-10-20 03:31:56 +0100

    Changed the folder structure
    Moved the presence code to a seperate folder to seperate it from the launcher code.

--- build/build.bat
+++ build/build.bat
@@ -35,35 +35,35 @@ if ["%1"]==[""] (
 call %BuildTools64%
 
 echo [BUILD]: COMPILING
-call cl.exe /nologo /EHsc /W4 /O2 /MD /std:c++17 /D_USRDLL /D_WINDLL /D_NDEBUG /Fo"bin\presence\intermediate\\" /I"..\source" /c ^
-/Tp"..\source\config.cpp" ^
-/Tp"..\source\main.cpp" ^
-/Tp"..\source\update.cpp" ^
-/Tp"..\source\discord\discord.cpp" ^
-/Tp"..\source\discord\presence.cpp" ^
-/Tp"..\source\parser\parser.cpp" ^
-/Tp"..\source\parser\rules.cpp" ^
-/Tp"..\source\thirdparty\t7api\cg.cpp" ^
-/Tp"..\source\thirdparty\t7api\com.cpp" ^
-/Tp"..\source\thirdparty\t7api\ddl.cpp" ^
-/Tp"..\source\thirdparty\t7api\demo.cpp" ^
-/Tp"..\source\thirdparty\t7api\g.cpp" ^
-/Tp"..\source\thirdparty\t7api\livestats.cpp" ^
-/Tp"..\source\thirdparty\t7api\livestorage.cpp" ^
-/Tp"..\source\thirdparty\t7api\lobbysession.cpp" ^
-/Tp"..\source\thirdparty\hook\kiero.cpp" ^
-/Tp"..\source\thirdparty\hook\hardware-breakpoint.cpp" ^
-/Tc"..\source\thirdparty\hook\minhook\hde\hde32.c" ^
-/Tc"..\source\thirdparty\hook\minhook\hde\hde64.c" ^
-/Tc"..\source\thirdparty\hook\minhook\trampoline.c" ^
-/Tc"..\source\thirdparty\hook\minhook\buffer.c" ^
-/Tc"..\source\thirdparty\hook\minhook\hook.c" ^
-/Tp"..\source\threads\attach.cpp" ^
-/Tp"..\source\threads\detach.cpp" 
+call cl.exe /nologo /EHsc /W4 /O2 /MD /std:c++17 /D_USRDLL /D_WINDLL /D_NDEBUG /Fo"bin\rpc\intermediate\\" /I"..\source\rpc" /c ^
+/Tp"..\rpc\source\config.cpp" ^
+/Tp"..\rpc\source\main.cpp" ^
+/Tp"..\rpc\source\update.cpp" ^
+/Tp"..\rpc\source\discord\discord.cpp" ^
+/Tp"..\rpc\source\discord\presence.cpp" ^
+/Tp"..\rpc\source\parser\parser.cpp" ^
+/Tp"..\rpc\source\parser\rules.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\cg.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\com.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\ddl.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\demo.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\g.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\livestats.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\livestorage.cpp" ^
+/Tp"..\rpc\source\thirdparty\t7api\lobbysession.cpp" ^
+/Tp"..\rpc\source\thirdparty\hook\kiero.cpp" ^
+/Tp"..\rpc\source\thirdparty\hook\hardware-breakpoint.cpp" ^
+/Tc"..\rpc\source\thirdparty\hook\minhook\hde\hde32.c" ^
+/Tc"..\rpc\source\thirdparty\hook\minhook\hde\hde64.c" ^
+/Tc"..\rpc\source\thirdparty\hook\minhook\trampoline.c" ^
+/Tc"..\rpc\source\thirdparty\hook\minhook\buffer.c" ^
+/Tc"..\rpc\source\thirdparty\hook\minhook\hook.c" ^
+/Tp"..\rpc\source\threads\attach.cpp" ^
+/Tp"..\rpc\source\threads\detach.cpp" 
 
 echo.
 echo [BUILD]: LINKING
-call link.exe /nologo /dll /subsystem:windows /out:"bin\presence\presence.dll" "bin\presence\intermediate\*.obj" "discord.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib"
+call link.exe /nologo /dll /subsystem:windows /out:"bin\rpc\presence.dll" "bin\rpc\intermediate\*.obj" "discord.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib"
 
 :pausethenend
 pause
--- build/copy-licenses.bat
+++ build/copy-licenses.bat
@@ -1,9 +1,9 @@
 @echo off
 
 echo [BUILD]: COPYING LICENSES
-copy "..\source\thirdparty\discord\discord-license"          "bin\licenses"
-copy "..\source\thirdparty\hook\hardware-breakpoint-license" "bin\licenses"
-copy "..\source\thirdparty\hook\kiero-license"               "bin\licenses"
-copy "..\source\thirdparty\hook\minhook\minhook-license"     "bin\licenses"
-copy "..\source\thirdparty\json\json-license"                "bin\licenses"
-copy "..\source\thirdparty\t7api\t7api-license"              "bin\licenses"
+copy "..\source\rpc\thirdparty\discord\discord-license"          "bin\licenses"
+copy "..\source\rpc\thirdparty\hook\hardware-breakpoint-license" "bin\licenses"
+copy "..\source\rpc\thirdparty\hook\kiero-license"               "bin\licenses"
+copy "..\source\rpc\thirdparty\hook\minhook\minhook-license"     "bin\licenses"
+copy "..\source\rpc\thirdparty\json\json-license"                "bin\licenses"
+copy "..\source\rpc\thirdparty\t7api\t7api-license"              "bin\licenses"
